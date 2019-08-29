be558af5c729b73d528a363c0fd0512d1676b7bd
helo <Jonathan.Hahn@t-online.de>
2019-08-29 16:19:41 +0200

    improve performance of thermal calculation

--- sim.py
+++ sim.py
@@ -1,7 +1,7 @@
 import numpy as np
-from scipy.interpolate import interp1d
 import numba
 
+from thermal import thermal
 
 # 
 g = 9.81
@@ -59,12 +59,14 @@ class Simulation:
         self.allstates[self.iteration] = newstate
 
     @property
+    def dstate(self):
+        return (self.state-self.laststate)/self.dt
+
+    @property
     def full_state(self):
         norm_state = [np.interp(statevar, [self.lower_bounds[i], self.upper_bounds[i]], [0, 1]) for i, statevar in enumerate(self.state)]
-        
-        dstate = (self.state-self.laststate)/self.dt
 
-        norm_dstate = [np.interp(statevar, [self.diff_lower_bounds[i], self.diff_upper_bounds[i]], [0, 1]) for i, statevar in enumerate(dstate)]
+        norm_dstate = [np.interp(statevar, [self.diff_lower_bounds[i], self.diff_upper_bounds[i]], [0, 1]) for i, statevar in enumerate(self.dstate)]
 
         return norm_state + norm_dstate
 
@@ -91,15 +93,15 @@ class Simulation:
         x, y, z, μ, φ = self.laststate
 
         # calculate thermal velocity
-        w = 10*thermal(x, y, z)
+        w = simple_thermal(x, y, z)
 
         # 
         if action == 0:
-            μ_new = np.clip(μ - self.μstep, -np.pi/2, np.pi/2)
+            μ_new = μ - self.μstep
         elif action == 1:
             μ_new = μ
         else:
-            μ_new = np.clip(μ + self.μstep, -np.pi/2, np.pi/2)
+            μ_new = μ + self.μstep
 
         v = self.v 
 
@@ -126,41 +128,6 @@ def simple_thermal(x, y, z, *args, **kwargs):
         return 2.0
     else:
         return 0.0
-
-
-def thermal(x, y, z, thermal_pos=[0.0, 0.0], z_i=1213.0, w_star=1.97):
-    """Calculate thermals following Allen 2006"""
-
-    if z>0.9*z_i:
-        return 0.0
-    else:
-        r = np.sqrt((x-thermal_pos[0])**2 + (y-thermal_pos[1])**2)
-        w_mean = w_star * (z/z_i)**3 * (1.0 - 1.1 * (z/z_i))
-        r2 = np.max((10.0, 0.102 * (z/z_i)**(1/3) * (1-0.25*(z/z_i)) * z_i))
-        r1 = (0.8 if r2>600 else 0.0011*r2 + 0.14)*r2
-        w_peak = 3 * w_mean * (r2**3 - r2**2*r1) / (r2**3 - r1**3)
-
-        w_D = 0.0 # TODO: calculate
-
-        k1, k2, k3, k4 = _get_ks(r1/r2)
-        w = w_peak * ( 1/(1+np.abs(k1*r/r2+k3)**k2) + k4 * r/r2 + w_D)
-
-        return w
-
-
-def _get_ks(rr):
-
-    rrs = np.array([0.14, 0.25, 0.36, 0.47, 0.58, 0.69, 0.80])
-    ks = np.array([[1.5352, 2.5826, -0.0113, 0.0008],
-                   [1.5265, 3.6054, -0.0176, 0.0005],
-                   [1.4866, 4.8354, -0.0320, 0.0001],
-                   [1.2042, 7.7904, 0.0848, 0.0001],
-                   [0.8816, 13.972, 0.3404, 0.0001],
-                   [0.07067, 23.994, 0.5689, 0.0002],
-                   [0.6189, 42.797, 0.7157, 0.0001]])
-
-    return interp1d(rrs, ks, kind='nearest', axis=0)(rr)
-
 
 def drag(v, μ):
     return c0 + c2 * (2*m*g)/(ρ*S*np.cos(μ))
